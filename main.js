008c857e4d094f8c063e060086b79967ce30de43
hopeingoderi <merry.grace44@yahoo.com>
2026-04-23 17:07:53 +0200

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.site-nav');
-if (navToggle && nav) {
-  navToggle.addEventListener('click', () => nav.classList.toggle('open'));
-}
