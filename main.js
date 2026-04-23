d7ee7188c274e0cdb880477fd926889a609bc36f
hopeingoderi <merry.grace44@yahoo.com>
2026-04-23 13:08:36 +0200

    Add files via upload
    michaeltech 23.04.2026

---
+++ main.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.site-nav');
+if (navToggle && nav) {
+  navToggle.addEventListener('click', () => nav.classList.toggle('open'));
+}
